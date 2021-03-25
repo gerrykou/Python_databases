939d7d19327e78a94009b0ce5545db184c7780ba
gerrykou <gerasimoskounadis@gmail.com>
2021-03-25 00:33:06 +0000

    init

---
+++ movies01.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+connection = sqlite3.connect('movies.db')
+
+cursor = connection.cursor()
+
+#cursor.execute("INSERT INTO Movies VALUES('Taxi driver', 'Martin Scorsese', 1976)")
+
+cursor.execute("SELECT * FROM Movies")
+
+print(cursor.fetchone())
+connection.commit()
+connection.close()
+
+
