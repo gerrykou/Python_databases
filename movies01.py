c152eecb88c0982fdeb13c26fb2800c615344970
gerrykou <gerasimoskounadis@gmail.com>
2021-03-26 00:46:52 +0000

    insert data from variable

--- movies01.py
+++ movies01.py
@@ -6,9 +6,24 @@ cursor = connection.cursor()
 
 #cursor.execute("INSERT INTO Movies VALUES('Taxi driver', 'Martin Scorsese', 1976)")
 
-cursor.execute("SELECT * FROM Movies")
+#cursor.execute("SELECT * FROM Movies")
 
-print(cursor.fetchone())
+# famousFilms = [('Pulp Fiction', 'Quentin Tarantino', 1994),
+# ('Back to the Future', 'Steven Spielberg', 1985),
+# ('Moonrise Kingdom','Wes Anderson', 2012)]
+
+# cursor.executemany('Insert INTO Movies VALUES (?,?,?)', famousFilms)
+
+# records = cursor.execute("SELECT * FROM Movies")
+
+# print(cursor.fetchall())
+
+release_year = (1985,)
+
+cursor.execute("SELECT * FROM Movies WHERE year = ?", release_year)
+
+print(cursor.fetchall())
+
 connection.commit()
 connection.close()
 
