6a315c19392f8995abbff6e1d5f1580e36c2553d
gerrykou <gerasimoskounadis@gmail.com>
2021-04-01 00:17:28 +0100

    sql defs

--- mysql-database.py
+++ mysql-database.py
@@ -11,12 +11,36 @@ def connect(db_name):
     except Error as e:
         print(e)
 
+def add_new_project(cursor, project_title, project_description, tasks_description, ):
+    project_data = (project_title, project_description)
+    cursor.execute('''INSERT INTO projects (title, description)
+        VALUES (%s, %s)''', project_data)
+
+    project_id = cursor.lastrowid
+
+    tasks_data = []
+    for description in tasks_description:
+        task_data = (project_id, description)
+        tasks_data.append(task_data)
+
+    cursor.executemany('''INSERT INTO tasks(project_id, description)
+        VALUES (%s, %s)''', tasks_data)
+
+
 if __name__ == '__main__':
     db = connect("projects")
     cursor = db.cursor()
 
+    tasks = ["Clean bathroom", "Clean kitchen","Clean living room",]
+    add_new_project(cursor, "Clean house", "Clean house by room", tasks)
+    db.commit()
+
     cursor.execute("SELECT * FROM projects")
     project_records = cursor.fetchall()
     print(project_records)
-
+  
+    cursor.execute("SELECT * FROM tasks")
+    tasks_records = cursor.fetchall()
+    print(tasks_records)
+    
     db.close()
